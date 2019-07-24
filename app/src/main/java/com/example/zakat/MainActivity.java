d2d603a4b31dbce2721263b0cf7bd8176416539b
jabrailahmed <jabraila.ames@gmail.com>
2019-07-23 20:40:03 -0600

    A new comment was added

--- app/src/main/java/com/example/zakat/MainActivity.java
+++ app/src/main/java/com/example/zakat/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    // This is a method that is overriden
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
